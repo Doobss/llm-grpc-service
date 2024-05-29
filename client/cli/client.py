72ee9d5c5405f6b8e027884f4b345fbd4d857775
Ian Bringe <42459194+Doobss@users.noreply.github.com>
2024-05-29 15:46:07 -0400

    fixing LLmClient params

--- client/cli/client.py
+++ client/cli/client.py
@@ -60,7 +60,7 @@ async def start_client(
     ) -> None:
     connect_attempts = 0
     max_retries = 10
-    client = LLmClient(user=username, host=host, port=port, secure=secure)
+    client = LLmClient(target=f'{host}:{port}')
     while max_retries > connect_attempts:
         try:
             connect_attempts = 0
